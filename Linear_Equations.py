259427f5d9e86c0b66f12045bf7448e4ddee77de
William <69966592+WilliamTheDev@users.noreply.github.com>
2022-05-26 21:32:12 +1000

    Update Linear_Equations.py

--- Linear_Equations.py
+++ Linear_Equations.py
@@ -7,8 +7,8 @@ def Linear_Equation(InputX: int, InputB: int):
    n[2] = (-([i for i in reversed(x)][1]))
    n[1] = (-([i for i in reversed(x)][2]))
 
-   X = (((n[0]*b[0]) + (n[1]*b[1])) / ((x[0]*x[3]) - (x[2]*x[1])))
-   Y = (((n[2]*b[0]) + (n[3]*b[1])) / ((x[0]*x[3]) - (x[2]*x[1])))
+   X = (n[0]*b[0] + n[1]*b[1]) / (x[0]*x[3] - x[2]*x[1])
+   Y = (n[2]*b[0] + n[3]*b[1]) / (x[0]*x[3] - x[2]*x[1])
    C = InputX[0]*X + InputX[1]*Y
    F = InputX[2]*X + InputX[3]*Y
 
